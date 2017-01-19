d94e801940dd5ffbcda8b8a2df5dadccfa1b6de8
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-19 11:10:30 -0600

    zip code grab

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 angular
    .module('anguweather', ['ngRoute'])
    .config(($routeProvider) => {
-      $routeProvider.when('/', {
-         controller : 'rootCtrl',
-         templateUrl: '/partials/root.html'
-      }
+      $routeProvider
+         .when('/', {
+            controller : 'rootCtrl',
+            templateUrl: '/app/partials/root.html'
+         })
+         .when('/weather/:zipcode', {
+            controller: 'weatherCtrl',
+            templateUrl: '/app/partials/weather.html'
+         })
    })
 
-   .controller('rootCtrl', function () {
+   .controller('rootCtrl', function ($scope, $location) {
       console.log('___welcome to the root controller___')
+      $scope.findWeather = () => $location.path(`/weather/${$scope.zipInput}`)
    })
+
+   .controller('weatherCtrl', function ($scope) {
+         console.log('___welcome to the weather controller___')
+      })
