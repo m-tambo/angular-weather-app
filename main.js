f172265bad24f59f14ab708d2018b35596f157fc
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-19 10:00:55 -0600

    adding controllers

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-let app = angular.module('anguweather', ['ngRoute'])
+angular
+   .module('anguweather', ['ngRoute'])
+   .config(($routeProvider) => {
+      $routeProvider.when('/', {
+         controller : 'rootCtrl',
+         templateUrl: '/partials/root.html'
+      }
+   })
+
+   .controller('rootCtrl', function () {
+      console.log('___welcome to the root controller___')
+   })
